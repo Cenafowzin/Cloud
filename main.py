f16c2408862f418b2ebcc3936d82e9b16dcec564
Rodrigo Dubeux <83378430+Cenafowzin@users.noreply.github.com>
2025-09-03 11:55:20 -0300

    pong

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello world!"}
+
+@app.post("/auth/me")
+def auth_me(user: str = Body(..., embed=True)):
+    return {"user": user, "ping": "pong"}
